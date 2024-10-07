b284cf783f0083b1bc1bf78b18861195219e616d
Thomas Kienlen <kommander@laposte.net>
2024-10-07 20:53:28 +0200

    🚧 Attempt to reorganize with structs

--- motion_speed.go
+++ motion_speed.go
@@ -13,6 +13,71 @@ const (
 	frameWindowSize = 2
 )
 
+type Frame struct {
+	number int
+	mat    gocv.Mat
+}
+
+func NewFrame(number int, mat gocv.Mat) *Frame {
+	if mat.Empty() {
+		log.Fatal("Frame is empty")
+	}
+
+	return &Frame{number: number, mat: mat}
+}
+
+func (f *Frame) Gray() *Frame {
+	gray := gocv.NewMat()
+
+	if f.mat.Empty() {
+		log.Fatal("Frame is empty")
+	}
+
+	gocv.CvtColor(f.mat, &gray, gocv.ColorBGRToGray)
+
+	return NewFrame(f.number, gray)
+}
+
+func (f *Frame) Clone() *Frame {
+	return NewFrame(f.number, f.mat.Clone())
+}
+
+func (f *Frame) Close() {
+	f.mat.Close()
+}
+
+type FrameBuffer struct {
+	average gocv.Mat
+}
+
+func NewFrameBuffer() *FrameBuffer {
+	return &FrameBuffer{
+		average: gocv.NewMat(),
+	}
+}
+
+func (fb *FrameBuffer) PixelsDiffCount(currentFrame *Frame) int {
+	if fb.average.Empty() {
+		currentFrame.mat.ConvertTo(&fb.average, gocv.MatTypeCV64F)
+	} else {
+		gocv.AccumulatedWeighted(currentFrame.mat, &fb.average, 0.5)
+	}
+
+	averageConverted := gocv.NewMat()
+	defer averageConverted.Close()
+
+	diff := gocv.NewMat()
+	defer diff.Close()
+
+	fb.average.ConvertTo(&averageConverted, gocv.MatTypeCV8U)
+
+	gocv.AbsDiff(currentFrame.mat, averageConverted, &diff)
+	gocv.Threshold(diff, &diff, 25, 255, gocv.ThresholdBinary)
+	nonZeroPixels := gocv.CountNonZero(diff)
+
+	return nonZeroPixels
+}
+
 func main() {
 	var videoPath string
 	var movementThreshold int
@@ -42,81 +107,50 @@ func main() {
 	}
 	fmt.Printf("Video frame rate: %.2f fps\n", fps)
 
-	frame := gocv.NewMat()
-	defer frame.Close()
-
-	gray := gocv.NewMat()
-	defer gray.Close()
-
-	average := gocv.NewMat()
-	defer average.Close()
-
-	diff := gocv.NewMat()
-	defer diff.Close()
+	frames := NewFrameBuffer()
+	// defer frames.Close()
 
 	frameIndex := 0
 	isMovementDetected := false
 	movementFrameCount := 0
 
-	frameBuffer := make([]gocv.Mat, 0, frameWindowSize)
-
 	for {
-		if ok := video.Read(&frame); !ok || frame.Empty() {
+		currentFrame := gocv.NewMat()
+		if ok := video.Read(&currentFrame); !ok || currentFrame.Empty() {
 			break
 		}
+
+		frame := NewFrame(frameIndex, currentFrame)
+		grayFrame := frame.Gray()
+		frame.Close()
+		defer grayFrame.Close()
+
 		frameIndex++
 
-		gocv.CvtColor(frame, &gray, gocv.ColorBGRToGray)
+		nonZeroPixels := frames.PixelsDiffCount(grayFrame)
 
-		if average.Empty() {
-			average = gocv.NewMatWithSize(gray.Rows(), gray.Cols(), gocv.MatTypeCV64F)
+		if nonZeroPixels < 0 {
+			log.Fatal("ko")
 		}
 
-		if len(frameBuffer) >= frameWindowSize {
-			frameBuffer[0].Close()
-			frameBuffer = frameBuffer[1:]
-		}
-		frameBuffer = append(frameBuffer, gray.Clone())
+		if nonZeroPixels > movementThreshold {
+			if !isMovementDetected {
+				isMovementDetected = true
 
-		if len(frameBuffer) == frameWindowSize {
-			average.SetTo(gocv.NewScalar(0.0, 0.0, 0.0, 0.0))
+				movementTime := float64(frameIndex) / fps
+				fmt.Printf("Motion starting at: %.2f seconds.\n", movementTime)
 
-			for _, f := range frameBuffer {
-				temp := gocv.NewMat()
-				f.ConvertTo(&temp, gocv.MatTypeCV64F)
-				gocv.AddWeighted(average, 1.0, temp, 1.0/float64(frameWindowSize), 0.0, &average)
-				temp.Close()
-			}
-
-			averageConverted := gocv.NewMat()
-			defer averageConverted.Close()
-			average.ConvertTo(&averageConverted, gocv.MatTypeCV8U)
-
-			gocv.AbsDiff(gray, averageConverted, &diff)
-
-			gocv.Threshold(diff, &diff, 25, 255, gocv.ThresholdBinary)
-
-			nonZeroPixels := gocv.CountNonZero(diff)
-
-			if nonZeroPixels > movementThreshold {
-				if !isMovementDetected {
-					isMovementDetected = true
-
-					movementTime := float64(frameIndex) / fps
-					fmt.Printf("Motion starting at: %.2f seconds.\n", movementTime)
-
-					if !gocv.IMWrite("motion-start.jpg", frame) {
-						log.Fatal("Unable to write image")
-					}
-				}
-
-				movementFrameCount++
-			} else if isMovementDetected {
-				isMovementDetected = false
-
-				if !gocv.IMWrite("motion-end.jpg", frame) {
+				if !gocv.IMWrite("motion-start.jpg", grayFrame.mat) {
 					log.Fatal("Unable to write image")
 				}
+			}
+
+			movementFrameCount++
+		} else if isMovementDetected {
+			isMovementDetected = false
+
+			if !gocv.IMWrite("motion-end.jpg", grayFrame.mat) {
+				log.Fatal("Unable to write image")
 			}
 		}
 	}
@@ -128,9 +162,5 @@ func main() {
 		fmt.Printf("Speed: %.2f km/h.\n", movementSpeed)
 	} else {
 		fmt.Println("Motion not detected")
-	}
-
-	for _, f := range frameBuffer {
-		f.Close()
 	}
 }
